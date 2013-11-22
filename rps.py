54a91159080fd72305be2037dc50d7583b58ab1d
Tomasz Pewiński <pewniak747@gmail.com>
2013-11-22 21:19:03 +0100

    reading from webcam

--- rps.py
+++ rps.py
@@ -3,3 +3,13 @@
 import cv2
 
 print("Rock Paper Scissors")
+
+cv2.namedWindow("mainWindow", cv2.CV_WINDOW_AUTOSIZE)
+camera = cv2.VideoCapture(0)
+
+def repeat():
+  ret, frame = camera.read()
+  cv2.imshow("mainWindow", frame)
+
+while True:
+  repeat()
